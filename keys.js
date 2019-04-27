7c6a08a0e8497ebd1c4a5e81a9575e53ba9088cb
TLI - Sushmita (SY) <sushmita@tli.com>
2019-04-27 16:34:10 -0400

    liri ready

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.1c3555fe7b0345e6b58ed07171f7c1e2,
-  secret: process.env.01091a47cd1f4414a246a0c26aa8e404
-};
+exports.spotifyKeys = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+}
